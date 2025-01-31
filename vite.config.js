f4c2cb78c7697eaff26f3e97c1cc578907661ba2
Davidgm09 <55807848+Davidgm09@users.noreply.github.com>
2025-01-31 18:59:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://davidgm09.github.io/TicTacToe.git"
+  base: "https://davidgm09.github.io/TicTacToe"
 })
